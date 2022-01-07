fa17a1f2c67b7ba43a3efa35d526f3b1973bed03
Steve Boyer <stephen.a.boyer@gmail.com>
2022-01-07 12:26:09 -0600

    Add mint() function that oveerrides ERC721Full.sol mint()

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "./ERC721Full.sol";
 
 
-// Here we create the ERC721 tokens for the tokens we will collect during the game
+// We create the ERC721 tokens for the tokens we will collect during the game
   // MemoryToken contract INHERITS from ERC721Full contract
 contract MemoryToken is ERC721Full {
   // 1st Naming Logic
@@ -21,6 +21,42 @@ contract MemoryToken is ERC721Full {
         // ERC721Full takes a name as an arg, so we simply use that here
   constructor () ERC721Full ("Memory Token", "MEMORY") public {
     // We leave this empty as there is no logic required at the moment
+  }
+
+
+  // We want to make the token mintable and distributable to other people
+    // The idea of the game is that it starts from zero - whenever a token is created the number of them starts at zero
+      // We want people to collect them
+  // Mint the token
+    // Takes two arguments
+      // 1. The address ("username") of the person we are going to give the token to
+      // 2. tokenURI is the reference to the image of the token (cats, etc.)
+  // Completing this function gives us a complete smart contract
+    // All we need for the contract are...
+      // A couple of variable values (the name and the symbol) <- in the constructor
+      // A special mint() function <- below
+    // This highlights how easy it is to create a smart contract using open zeppelin templates
+  function mint(address _to, string memory _tokenURI) public returns (bool) {
+    // Here we have to override the mint function in ERC721Full.sol because we want to create our own and do it a little bit differently
+      // The standard function TAKES a tokenId but we want to MAKE a tokenId
+
+    // Set the token Id
+      // We inherit the totalSupply() function from ERC721Full.sol
+      // This function returns the number of tokens that already exist
+      // We use this number, and increment it to create a new tokenId without accidentally overriding a tokenId that already exists
+    uint _tokenId = totalSupply().add(1);
+    // Using _mint here is how we pass in the mint function from the contract we have inherited from (ERC721Full.sol)
+      // The inherited function takes the address _to (the address of the person we are sending the token to) and the tokenId we set right above
+      // So this is what we pass it
+    _mint(_to, _tokenId);
+
+    // Set the tokenURI
+      // We inherit the _setTokenURI function from ERC721Full.sol
+      // The inherited function takes the tokenId (that we set above) and the uri for the token
+    _setTokenURI(_tokenId, _tokenURI);
+
+    // Return true because the function returns a bool
+    return true
   }
 
 }
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -6,7 +6,10 @@ require('chai')
   .should()
 
 
-// Contract function used to create the tests
+// contract function used to create the tests
+  // Takes two arguments
+    // 1. Name of the contract
+    // 2. A function that gives you a parameter with a list of accounts that your environment provided for testing
 contract('Memory Token', (accounts) => {
   // First thing we want to do is declare the token...
     // This is simply a var that we can asign the token to when we fetch it back from the blockchain <- for example when you are interacting with the Truffle console you want to be able to use this var
@@ -15,14 +18,19 @@ contract('Memory Token', (accounts) => {
   // Here we create a hook using a before() block so that BEFORE every test, the code in the block will be run
     // This is especially helpful for repeat functions - in this case we need to fetch the token for each test, so we just throw it in this hook
 
+  // Fetch the token from the blockchain before each test
+    // Here we are accessing an instance of the contact which is the artifact
   before(async () => {
     // We use the exact same code that we do in the Truffle console 
     // We store the value of the contract in the token var
     token = await MemoryToken.deployed()
   })
 
-
+  // describe() takes two arguments
+    // 1. Description of the test
+    // 2. The actual test
   describe('deployment', async () => {
+    // Here we test if it deploys successfully and check the basic attributes of the token
     it('deploys successfully', async () => {
 
 
@@ -41,10 +49,31 @@ contract('Memory Token', (accounts) => {
       // Tell me that it is not undefined
       assert.notEqual(address, undefined)
     })
-    // We want to fetch the memory token before we write more tests
+    // Name test
+    // We want to fetch the memory token name before we write more tests
     it('has a name', async () => {
       const name = await token.name()
       assert.equal(name, 'Memory Token')
     })
+    // Symbol test
+    it('has a symbol', async () => {
+      const symbol = await token.symbol()
+      assert.equal(symbol, 'MEMORY')
+    })
   })
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
